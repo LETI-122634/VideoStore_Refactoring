3a44b1d30975e345732a0ff5f72d29a613be2abd
LETI-122636 <mfrta@iscte-iul.pt>
2025-10-24 10:56:10 +0100

    Resoluçao do V8

--- src/ficha3/v7/Price.java
+++ src/ficha3/v7/Price.java
@@ -1,6 +1,8 @@
 package ficha3.v7;
 
 public abstract class Price {
+    public enum Code { REGULAR, NEW_RELEASE, CHILDRENS }
+
     public abstract double getAmount(int daysRented);
 
     public int getFrequentRentalPoints(int daysRented) {

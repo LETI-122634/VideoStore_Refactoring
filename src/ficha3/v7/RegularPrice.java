f9fd098077da2d06dc1d78475b01a9aa0a2029c5
LETI_122634 <hclse@iscte-iul.pt>
2025-10-24 10:50:29 +0100

    V7, Switch Statement: Adicionamos a classe abstrata Price e 3 subclasses a esta: RegularPrice, NewReleasePrice e ChildrenPrice. Alteramos a classe Movie desaparecendo o switch statment

---
+++ src/ficha3/v7/RegularPrice.java
@@ -0,0 +1,11 @@
+package ficha3.v7;
+
+public class RegularPrice extends Price {
+    @Override
+    public double getAmount(int daysRented) {
+        double result = 2;
+        if (daysRented > 2)
+            result += (daysRented - 2) * 1.5;
+        return result;
+    }
+}
